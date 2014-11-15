61638780f2b8ab1cd9f618f921c1ae5fa250f650
Eric Goldberg <eric.goldberg@gmail.com>
2014-11-14 17:46:45 -0800

    Added a restart xcode instruction after script completes

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ echo 'Backing up the original cursor that ships with xcode to ./backup-DVTIbeamC
 cp /Applications/Xcode.app/Contents/SharedFrameworks/DVTKit.framework/Resources/DVTIbeamCursor.tiff ./backup-DVTIbeamCursor.tiff
 echo 'Copying the improved ibeam cursor to the correct location'
 sudo cp DVTIbeamCursor.tiff /Applications/Xcode.app/Contents/SharedFrameworks/DVTKit.framework/Resources/DVTIbeamCursor.tiff
-echo 'Done copying , have fun!'
+echo 'Done copying - restart Xcode and have fun!'
 
