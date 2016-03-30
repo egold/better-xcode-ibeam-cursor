dbcd8fe1b55dd520cd7a895dfa44e84694961f8b
Eric Baker <eric.baker@hyatt.com>
2016-03-30 10:09:49 -0500

    Patch Assets.car rather than overwrite.

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ XCODE_BETA_APP_PATH=$APPLICATIONS_PATH/$XCODE_BETA_APP_DIR_NAME/$XCODE_RESOURCE_
 IBEAM_TIFF_FILE=DVTIbeamCursor.tiff
 ASSETS_CAR_FILE=Assets.car
 
+XCODE_7_3_ASSETS_CAR_SHA=8e362f0cd2ee6306908b5275a721373ce8451a39
+
+GITHUB_URL=https://raw.githubusercontent.com/egold/better-xcode-ibeam-cursor/master
+GITHUB_PATCHES_PATH=patches
+
 function update_cursor_for_xcode() {
     xcode_path=$1
     show_failure_msg=$2
@@ -30,20 +35,29 @@ function update_cursor_for_xcode() {
 }
 
 function download_replacement_file() {
-    if [ -e "${xcode_path}/${IBEAM_TIFF_FILE}" ];
+    patch_file=false
+    if [ -e "${xcode_path}/${IBEAM_TIFF_FILE}" ]
     then
         resource_file_name=$IBEAM_TIFF_FILE
-    elif [ -e "${xcode_path}/${ASSETS_CAR_FILE}" ];
+    elif [ -e "${xcode_path}/${ASSETS_CAR_FILE}" ]
     then
-        resource_file_name=$ASSETS_CAR_FILE
+        resource_sha=`shasum "${xcode_path}/${ASSETS_CAR_FILE}" | awk 'BEGIN { FS = " " } ; { print $1 }'`
+        resource_file_name="${ASSETS_CAR_FILE}-${resource_sha}.bspatch"
+        patch_file=true
     else
         return -1
     fi
 
-    echo "Downloading new cursor file"
     if [ -e "tmp/${resource_file_name}" ]; then rm "/tmp/${resource_file_name}"; fi
 
-    curl -o "/tmp/${resource_file_name}" "https://raw.githubusercontent.com/egold/better-xcode-ibeam-cursor/master/${resource_file_name}"
+    if [ $patch_file = true ]
+    then
+        echo "Downloading cursor patch"
+        curl -o "/tmp/${resource_file_name}" "${GITHUB_URL}/${GITHUB_PATCHES_PATH}/${resource_file_name}"
+    else
+        echo "Downloading new cursor file"
+        curl -o "/tmp/${resource_file_name}" "${GITHUB_URL}/${resource_file_name}"
+    fi
 
     if [ ! -s "/tmp/${resource_file_name}" ]
     then
@@ -66,8 +80,14 @@ function backup_original_file() {
 }
 
 function install_replacement_file() {
-    echo "Copying the improved ibeam cursor to the correct location"
-    sudo cp "/tmp/${resource_file_name}" "${xcode_path}/${resource_file_name}"
+    if [ $patch_file = true ]
+    then
+        echo "Patching in the improved ibeam cursor"
+        sudo bspatch "${xcode_path}/${ASSETS_CAR_FILE}" "${xcode_path}/${ASSETS_CAR_FILE}" "/tmp/${resource_file_name}"
+    else
+        echo "Copying the improved ibeam cursor to the correct location"
+        sudo cp "/tmp/${resource_file_name}" "${xcode_path}/${resource_file_name}"
+    fi
 }
 
 function cleanup() {
