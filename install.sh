c55ac922409b866b3a550c3df3f07abfc7e6cd76
Eric Baker <eric.baker@hyatt.com>
2016-04-27 10:03:49 -0500

    Adds support for Xcode 7.3.1

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Constants
-GITHUB_URL="https://raw.githubusercontent.com/egold/better-xcode-ibeam-cursor/master"
+GITHUB_URL="https://raw.githubusercontent.com/ebaker355/better-xcode-ibeam-cursor/master"
 GITHUB_PATCHES_PATH="patches"
 TMP_PATH="/tmp"
 IBEAM_TIFF_FILENAME="DVTIbeamCursor.tiff"
@@ -12,6 +12,12 @@ XCODE_7_3_ASSETS_CAR_ORIGINAL_SHA="8e362f0cd2ee6306908b5275a721373ce8451a39"
 XCODE_7_3_ASSETS_CAR_PATCH_FILENAME="Assets.car-Xcode-7.3.bspatch"
 XCODE_7_3_ASSETS_CAR_PATCH_SHA="f5897929a05cf5f17ce9b31e2360afe2d6b5f7fe"
 XCODE_7_3_ASSETS_CAR_PATCHED_SHA="33c3b358988a25907f181640b8baef3ab603d7c2"
+
+XCODE_7_3_1_ASSETS_CAR_ORIGINAL_SHA="b9b12ffcbf0be9a8fbbaa043a90cd0317f4a316d"
+XCODE_7_3_1_ASSETS_CAR_PATCH_FILENAME="Assets.car-Xcode-7.3.1.bspatch"
+XCODE_7_3_1_ASSETS_CAR_PATCH_SHA="855263f05cb6111e4f91cec3464e479124665a26"
+XCODE_7_3_1_ASSETS_CAR_PATCHED_SHA="2da32d5b87a2d16d5cc4d49922a517ae32e3cc77"
+
 DVTIBEAM_CURSOR_TIFF_SHA="4b9d55d84e05cb1dff0815acc0a548f40bf8da27"
 
 function update_cursor_for_xcode() {
@@ -51,20 +57,30 @@ function determine_required_patch_file_for_xcode() {
         if [ $sha = $XCODE_7_3_ASSETS_CAR_ORIGINAL_SHA ]; then
             #       Yes. We need the patch file for it.
             file_to_download=$XCODE_7_3_ASSETS_CAR_PATCH_FILENAME
+            file_to_download_expected_sha=$XCODE_7_3_ASSETS_CAR_PATCH_SHA
+            patched_file_expected_sha=$XCODE_7_3_ASSETS_CAR_PATCHED_SHA
+        elif [ $sha = $XCODE_7_3_1_ASSETS_CAR_ORIGINAL_SHA ]; then
+            file_to_download=$XCODE_7_3_1_ASSETS_CAR_PATCH_FILENAME
+            file_to_download_expected_sha=$XCODE_7_3_1_ASSETS_CAR_PATCH_SHA
+            patched_file_expected_sha=$XCODE_7_3_1_ASSETS_CAR_PATCHED_SHA
         else
             #       No. The file may have already been patched. Display a message, then return.
             if [ $sha = $XCODE_7_3_ASSETS_CAR_PATCHED_SHA ]; then
                 echo "The patch has already been applied."
             else
-                echo "File checksum mismatch. Patch cannot be applied."
+                echo "File checksum mismatch."
+                echo "${ASSETS_CAR_FILENAME} - ${sha}"
+                echo "Patch cannot be applied."
             fi
         fi
     else
         #     No. Does the DVTIbeamCursor.tiff file exist?
         if [ -e "${xcode_path}/${IBEAM_TIFF_FILENAME}" ]; then
             #   Yes. We need the replacement file for it. We're going to be replacing the file rather than patching.
-            file_to_download=$IBEAM_TIFF_FILENAME;
             replace_file_mode=true
+            file_to_download=$IBEAM_TIFF_FILENAME;
+            file_to_download_expected_sha=$DVTIBEAM_CURSOR_TIFF_SHA
+            patched_file_expected_sha=$DVTIBEAM_CURSOR_TIFF_SHA
         else
             #     No. We have no file to patch. Is this a newer version of Xcode? Display a message, then return.
             echo "Unable to determine which file needs to be patched. Is this a newer version of Xcode?"
@@ -80,17 +96,15 @@ function download_patch_file() {
     # Download the patch file.
     noun="patch"
     download_url="${GITHUB_URL}/${GITHUB_PATCHES_PATH}"
-    expected_sha=$XCODE_7_3_ASSETS_CAR_PATCH_SHA
     if [ $replace_file_mode = true ]; then
         noun="replacement"
         download_url="${GITHUB_URL}"
-        expected_sha=$DVTIBEAM_CURSOR_TIFF_SHA
     fi
     echo "Downloading cursor ${noun} file to ${download_destination_path}"
     curl -o "${download_destination_path}" "${download_url}/${file_to_download}"
     # Does the downloaded file's SHA match what we were expecting?
     sha=($(shasum "${download_destination_path}"))
-    if [ $sha = $expected_sha ]; then
+    if [ $sha = $file_to_download_expected_sha ]; then
         #   Yes. Continue.
         echo "Downloaded successfully."
     else
@@ -123,10 +137,8 @@ function backup_original_file() {
 }
 
 function apply_patch() {
-    expected_sha=$XCODE_7_3_ASSETS_CAR_PATCHED_SHA
     # We are now ready to patch or replace Xcode's file. Apply the patch/replacement.
     if [ $replace_file_mode = true ]; then
-        expected_sha=$DVTIBEAM_CURSOR_TIFF_SHA
         sudo rm "${xcode_path}/${original_file}"
         sudo mv "${download_destination_path}" "${xcode_path}/${original_file}"
     else
@@ -134,7 +146,7 @@ function apply_patch() {
     fi
     # Does the patched file's SHA match what we were expecting?
     sha=($(shasum "${xcode_path}/${original_file}"))
-    if [ $sha = $expected_sha ]; then
+    if [ $sha = $patched_file_expected_sha ]; then
         #   Yes. Patch was successful. Display message, then continue.
         echo "Xcode has been patched successfully. Please restart Xcode and have fun!"
     else
